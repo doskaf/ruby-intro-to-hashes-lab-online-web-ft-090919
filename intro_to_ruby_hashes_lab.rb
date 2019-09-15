4f27dca3062b0329ff29b85943a68ee24a48bf30
Dominique Skaf <doskaf@yahoo.com>
2019-09-15 05:09:38 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -59,7 +59,7 @@ def monopoly_with_fourth_tier
 	      {"mortgage_value" => "$400",
 	      },
 	    :shortline => 
-	      {"mortgage_value" => "",
+	      {"mortgage_value" => "$",
 	      }
 	    }, 
 	  :rent_in_dollars => 
